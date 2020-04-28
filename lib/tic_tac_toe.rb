0f1381694f0b283c17da5cfde72aba4f596280ae
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-28 18:12:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -154,7 +154,6 @@ class TicTacToe
   ##
   def turn
     puts "Please enter 1-9:"
-    # input = gets.strip
     index = input_to_index(gets.strip)
     valid_move?(index) ? move(index, current_player) : turn
   end
