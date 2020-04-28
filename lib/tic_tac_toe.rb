699f858ddfe532868387c7c3ae09d7fa383705ff
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-28 17:41:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,8 +79,8 @@ class TicTacToe
   ##
   # Is position at 'index' taken?
   ##
-  def position_taken?(board, index)
-    !(board[index].nil? || board[index] == " ")
+  def position_taken?
+    !(@board[@index].nil? || @board[@index] == " ")
   end
 
   ##
